49dbbe1ab0282ef4470a113d7c1a6e7849af8076
removedporn <86824510+removedporn@users.noreply.github.com>
2022-11-02 05:19:01 +0800

    Delete api directory

--- api/models/orders.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, //product schemaya bağlı.
-    quantity: { type: Number, required: true, default: 1 },
-});
-
-module.exports = mongoose.model('Order', orderSchema);
